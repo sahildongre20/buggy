edb446a11bf86e2d437ae8b4bbc57fe2bf4330e4
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2023-04-27 00:08:32 +0530

    added test mail

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -121,7 +121,7 @@ LOGOUT_REDIRECT_URL = "/login"
 LOGIN_REDIRECT_URL = "/login"
 LOGIN_URL = "/login"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.eu.mailgun.org'
+EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
@@ -129,7 +129,7 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
-GRAPH_MODELS ={
-'all_applications': True,
-'graph_models': True,
+GRAPH_MODELS = {
+    'all_applications': True,
+    'graph_models': True,
 }
